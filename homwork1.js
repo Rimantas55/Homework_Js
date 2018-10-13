07f4ca76c8dadfbdf30dcd5fd97104ebb086c7eb
Rimantas55 <43905835+Rimantas55@users.noreply.github.com>
2018-10-13 12:43:37 +0300

    pridejau .js .css .html files
    1) Bandau sukurti reposiutoriume Homework_Js github'e - patikrinti ar susikure

---
+++ homwork1.js
@@ -0,0 +1,18 @@
+console.log('Aplikacija veikia');
+//document.getElementById("plus").innerHTML = "Rimas";
+var = document.getElementById('nomer');
+
+function plus (){
+	var value = nomer.innerHTML;
+	value++;
+	nomer.innerHTML = value;
+	console.log('up');
+	
+}
+
+ function minus (){
+ 	var value = nomer.innerHTML;
+	value--;
+	nomer.innerHTML = value;
+	console.log('down');
+}
